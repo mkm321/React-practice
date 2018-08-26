8fedaafd2be5a60d96b79cdb2817685784e6db42
Mohit <mmehndiratta@tavisca.com>
2018-08-27 02:32:34 +0530

    added till first react component

---
+++ react project/source/components/homePage.js
@@ -0,0 +1,13 @@
+"use strict";
+var React = require('react');
+var Home = React.createClass({
+    render: function(){
+        return (
+            <div className="jumbotron">
+                <h1>React Admin</h1>
+                <p> React, React flux for this</p>
+            </div>
+        );
+    }
+});
+module.exports = Home;
